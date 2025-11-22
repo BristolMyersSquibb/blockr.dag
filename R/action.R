3151925ec552713e6f99369479945c8752f5a9b6
DivadNojnarg <dgranjon@ymail.com>
2025-11-22 16:31:21 +0100

    handle append block

--- R/action.R
+++ R/action.R
@@ -177,8 +177,9 @@ append_block_action <- new_action(
             board = board$board
           )
         )
-
-        g6_get_nodes(g6_proxy(graph_id()) , trigger())
+        
+        # Get trigger coordinates
+        g6_get_nodes(proxy , trigger())
       }
     )
 
@@ -187,9 +188,12 @@ append_block_action <- new_action(
       {
         req(input$append_block_selection)
 
+        trigger_style <- proxy$session$input[[paste0(graph_id(), "-", trigger(), "-state")]]$style
+
         new_blk <- create_block_with_name(
           input$append_block_selection,
-          chr_ply(board_blocks(board$board), block_name)
+          chr_ply(board_blocks(board$board), block_name),
+          coords = list(x = trigger_style$x, y = trigger_style$y + 150) # Offset below trigger block
         )
 
         res <- block_input_select(
