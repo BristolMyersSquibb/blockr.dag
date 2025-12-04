956234be7f1974b53c2b11ac70537351b7535da9
DivadNojnarg <dgranjon@ymail.com>
2025-12-04 09:08:40 +0100

    wip test ext ui

---
+++ tests/testthat/test-ext.R
@@ -0,0 +1,33 @@
+library(testthat)
+
+empty_board <- new_board()
+non_empty_board <- new_board(
+  blocks = list(
+    new_dataset_block()
+  )
+)
+
+
+test_that("ext_ui works", {
+  ui <- dag_ext_ui("test", empty_board)
+  expect_s3_class(ui, "shiny.tag.list")
+
+  empty_state_container <- htmltools::tagQuery(ui)$find(
+    ".dag-empty-state"
+  )$selectedTags()[[1]]
+
+  expect_null(empty_state_container$attribs$style)
+
+  # Non empty board
+  ui <- dag_ext_ui("test", non_empty_board)
+  empty_state_container <- htmltools::tagQuery(ui)$find(
+    ".dag-empty-state"
+  )$selectedTags()[[1]]
+
+  expect_identical(empty_state_container$attribs$style, "display: none;")
+
+  # Custom dependencies
+  deps <- htmltools::findDependencies(ui)
+  dep_names <- chr_ply(deps, `[[`, "name")
+  expect_contains(dep_names, c("rm-selection", "dag-empty-state"))
+})
