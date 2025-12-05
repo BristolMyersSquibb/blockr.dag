2d5e77f582c7254511e717510e61043f9ab81c85
DivadNojnarg <dgranjon@ymail.com>
2025-12-05 17:05:46 +0100

    basic e2e

---
+++ tests/testthat/test-e2e.R
@@ -0,0 +1,31 @@
+library(shinytest2)
+test_that("sample_app works", {
+  appdir <- system.file(package = "blockr.dag", "examples/empty")
+  local_app_support(appdir)
+
+  app <- AppDriver$new(appdir, name = "empty-app", seed = 4323)
+
+  app$expect_values()
+
+  # Add a new block: with custom id
+  app$click(selector = ".g6-toolbar-item[value=\"add_block\"")
+  app$wait_for_idle()
+  app$set_inputs(
+    `board-dag_extension-tool_add_block-add_block_selection` = "dataset_block"
+  )
+  app$wait_for_idle()
+  app$click(
+    selector = "#board-dag_extension-tool_add_block-block-advanced-toggle"
+  )
+  app$wait_for_idle()
+  app$set_inputs(
+    `board-dag_extension-tool_add_block-add_block_id` = "super_data_block"
+  )
+  app$wait_for_idle()
+  app$click("board-dag_extension-tool_add_block-add_block_confirm")
+  app$expect_values()
+
+  # TBD: continue testing more interactions
+
+  app$stop()
+})
