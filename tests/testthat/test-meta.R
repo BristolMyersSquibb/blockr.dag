21ab42df98fcdde0b12c5066fd7766743c5e152e
DivadNojnarg <dgranjon@ymail.com>
2026-02-26 10:26:09 +0100

    hotfix: reintroduce lost changes

--- tests/testthat/test-meta.R
+++ tests/testthat/test-meta.R
@@ -2,27 +2,53 @@ new_dummy_blk <- function(...) {
   new_data_block(
     function(id) {},
     function(id) tagList(),
+    block_metadata = list(),
     class = "test_block",
     ...
   )
 }
 
-test_that("blk_category works", {
-  blk <- new_dataset_block()
-  categ <- blk_category(blk)
-  expect_type(categ, "character")
+test_that("blks_color works", {
+  blk1 <- new_dataset_block()
+  cat1 <- blks_color(blk1)
+
+  expect_type(cat1, "character")
+  expect_length(cat1, 1L)
 
   # Unknown block
-  categ <- blk_category(new_dummy_blk())
-  expect_identical(categ, default_category())
+  default <- blockr.dock::blk_color(default_category())
+
+  blk2 <- new_dummy_blk()
+  cat2 <- blks_color(blk2)
+
+  expect_identical(cat2, default)
+
+  cat3 <- blks_color(c(blk1, blk2))
+
+  expect_type(cat3, "character")
+  expect_length(cat3, 2L)
 })
 
-test_that("blk_icon works", {
-  blk <- new_dataset_block()
-  ic <- blk_icon(blk)
-  expect_type(ic, "character")
+test_that("blks_icon works", {
+  blk1 <- new_dataset_block()
+  icn1 <- blks_icon(blk1)
+
+  expect_type(icn1, "character")
+  expect_length(icn1, 1L)
 
   # Unknown block
-  ic <- blk_icon(new_dummy_blk())
-  expect_identical(ic, default_icon(default_category()))
+  default <- blockr.dock::blk_icon_data_uri(
+    default_icon(),
+    blockr.dock::blk_color(default_category())
+  )
+
+  blk2 <- new_dummy_blk()
+  icn2 <- blks_icon(new_dummy_blk())
+
+  expect_identical(icn2, default)
+
+  icn3 <- blks_icon(c(blk1, blk2))
+
+  expect_type(icn3, "character")
+  expect_length(icn3, 2L)
 })
